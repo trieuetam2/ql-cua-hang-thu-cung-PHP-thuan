001d3edc18e15c30bc1721e5fe33865e5f6f27b0
Tin <nguyenteo1020@gmail.com>
2023-03-29 13:08:59 +0700

    up lai

--- function/config.php
+++ function/config.php
@@ -1,17 +1,10 @@
 <?php
 //ket noi toi database
 
-<<<<<<< HEAD
 	$servername = 'localhost';
 	$username = 'root';
 	$password = '';
 	$dbname = 'shop';
-=======
-$servername = 'localhost';
-$username = 'root';
-$password = 'nguyen thanh tin';
-$dbname = 'shop';
->>>>>>> 8d21157ac9c7ba749a927fa636ed8f0e76148e53
 
 //connect database
 $conn = mysqli_connect($servername, $username, $password, $dbname);
